c55b94857cd7cfebd1ee56bbfdafcbb100735ba2
Sana Matusala <matusalasana@gmail.com>
2025-11-24 22:06:23 +0300

    gender filtering logic done

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,9 +1,11 @@
+
 interface Props{
     onSortChange: (value:string) => void;
+    onClickGenderCheckbox: (value:string) =>void;
     
 }
 
-function Filters( { onSortChange }: Props ) {
+function Filters( { onSortChange, onClickGenderCheckbox }: Props ) {
 
     const sort = [
         {label: 'Sort By', value: ''},
@@ -11,6 +13,13 @@ function Filters( { onSortChange }: Props ) {
         {label: 'Price: High to Low', value: 'high-to-low-price'},
         {label: 'Price: Low to High', value: 'low-to-high-price'},
         {label: 'Number of reviews', value: 'review'},
+    ]
+
+
+    const gender = [
+        { label: 'Men', value: 'men' },
+        { label: 'Women', value: 'women' },
+        { label: 'Kids', value: 'kids' },
     ]
 
   return (
@@ -21,6 +30,16 @@ function Filters( { onSortChange }: Props ) {
                     <option key={sortBy.value} value={sortBy.value}>{sortBy.label}</option>
                 ) )}
             </select>
+        </div>
+
+        <div className="mt-5">
+            <p>Gender</p>
+            { gender.map( (sex) => (
+                <div key={sex.value} >
+                    <input onChange={ () => onClickGenderCheckbox(sex.value)}  type="checkbox" id={sex.value} />
+                    <label htmlFor={sex.value}>{sex.label}</label>
+                </div>
+            )) }
         </div>
         
     </div>
--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -10,53 +10,92 @@ function Collection() {
 
     const {products} = useShop()!
     const [sortBy, setSortBy] = useState('')
-    const [sortedItems, setSortedItems] = useState<Product[]>([])
+    // const [sortedItems, setSortedItems] = useState<Product[]>([])
+    const [filteredItems, setFilteredItems] = useState<Product[]>([])
+
+    const [checkboxResults, setCheckboxResults] = useState([
+        { gender: 'men', status: false},
+        { gender: 'women', status: false},
+        { gender: 'kids', status: false},
+    ])
+
+    const handleCheckboxToggle = (value: string) => {
+        setCheckboxResults(prev => 
+            prev.map(item => 
+                item.gender === value 
+                    ? { ...item, status: !item.status } 
+                    : item
+            )
+        )
+    }
+
 
     useEffect(() => {
-    if (!products.length) {
-        setSortedItems([]);
-        return;
-    }
 
-    const sortedProducts = [...products];
+        if (!products.length) {
+            setFilteredItems([]);
+            return;
+        }
+
+        // const sortedProducts = [...products];
+        
+        
+        // switch(sortBy) {
+        //     case "name":
+        //         sortedProducts.sort((a, b) => 
+        //             a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+        //         break;
+        //     case "high-to-low-price":
+        //         sortedProducts.sort((a, b) => 
+        //             b.price - a.price);
+        //         break;
+        //     case "low-to-high-price":
+        //         sortedProducts.sort((a, b) => 
+        //             a.price - b.price);
+        //         break;
+        //     case "review":
+        //         sortedProducts.sort((a, b) => 
+        //             b.price - a.price);
+        //         break;
+        //     default:
+        //         break;
+        // }
+        
+
+        const activeValues = checkboxResults
+            .filter(item => item.status === true)
+            .map(item => item.gender)
+
+            console.log(activeValues)
+
+        if (activeValues.length===0) {
+            setFilteredItems([...products])
+        }
+        else {
+            const genderFilteredItems = products.filter( product => activeValues.includes(product.category.toLowerCase()))
+            setFilteredItems(genderFilteredItems)
+        }
+
+        // setSortedItems(sortedProducts);
+
+        }, [ products, checkboxResults]);
     
-    switch(sortBy) {
-        case "name":
-            sortedProducts.sort((a, b) => 
-                a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-            break;
-        case "high-to-low-price":
-            sortedProducts.sort((a, b) => 
-                b.price - a.price);
-            break;
-        case "low-to-high-price":
-            sortedProducts.sort((a, b) => 
-                a.price - b.price);
-            break;
-        case "review":
-            sortedProducts.sort((a, b) => 
-                b.price - a.price);
-            break;
-        default:
-            break;
-    }
     
-    setSortedItems(sortedProducts);
-}, [sortBy, products]);
-    
-    
-    console.log(sortBy)
     return (
         <div className="pt-30">
 
             <div className="px-15">
                 <Title text1="YOUR" text2="COLLECTION" />
+
+                <p onClick={() => setFilteredItems(products)}>Clear all filters</p>
+
                 <Filters 
                     onSortChange={(value)=> setSortBy(value) } 
+                    onClickGenderCheckbox={ handleCheckboxToggle }
                 />
                 <div className="grid grid-cols-3 max-sm:grid-cols-1 lg:grid-cols-4 xl:grid-cols-5 gap-5">
                     
-                    {sortedItems.map((product) => {
+                    {filteredItems.map((product) => {
                         return (
                             <ProductItem
                             key={product._id}
