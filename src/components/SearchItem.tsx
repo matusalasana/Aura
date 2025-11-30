cc9d05fd92fc4b17ae5bd545391eac747aca5331
Sana Matusala <matusalasana@gmail.com>
2025-11-30 09:23:40 +0300

    bug fixed

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -13,7 +13,6 @@ function SearchItem({ onSearch }:Props) {
 
 
   return (
-    <div className="fixed bg-linear-to-r from-gray-200 to-gray-100 left-0 right-0 top-20 pt-5 px-2 z-10">
     <div className={`relative pr-5 mb-5 rounded-lg flex justify-center
     ${ text
         ? 'block'
@@ -22,7 +21,6 @@ function SearchItem({ onSearch }:Props) {
             <input type="text" onChange={ (e) => onSearch(e.currentTarget.value) } placeholder="search here . . ." className="bg-slate-900/15 border rounded-full px-7 py-0.5 w-full  " />
             <Search size={20} className="absolute top-1.5 left-1"/>
             <CgClose onClick={ () => closeSearch()} size={20} className="absolute top-0.5 -right-1 cursor-pointer text-red-500 hover:text-red-700"/>
-    </div>
     </div>
   )
 }
