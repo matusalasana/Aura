1924d8592525fff85848d7678e5c944d2bc92a73
Sana Matusala <matusalasana@gmail.com>
2025-12-07 00:51:51 +0300

    item card updated

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,8 +1,9 @@
 
 import { useShop } from "../context/ShopContext"
 import { Link } from "react-router-dom"
-import { ShoppingBag } from "lucide-react";
 import { BsHeart, BsHeartFill, BsStarFill, BsStarHalf } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+import { CgShare } from "react-icons/cg";
 
 interface Props {
     name: string;
@@ -34,11 +35,6 @@ function ProductItem({ name, productId, imgURL, price, category }: Props) {
                             className="p-2 rounded-2xl w-full h-full object-cover hover:scale-110 transition-transform duration-500"
                         />
                         </Link>
-                        <div className="absolute top-2 left-2">
-                            <span className="bg-gray-600 px-2 py-1 rounded-full shadow-lg text-xs text-white">
-                                {category}
-                            </span> 
-                        </div>
                         <div className="absolute top-3 right-3">
 
                             <button onClick={ () => (handleToggle(), addToWishList(productId))} className="cursor-pointer flex items-center gap-1 bg-white/90 hover:bg-white px-2 py-1 rounded-full shadow-lg">
@@ -58,40 +54,56 @@ function ProductItem({ name, productId, imgURL, price, category }: Props) {
 
                 <div className="p-4">
 
-                    <div className="mb-3">
+                    <div className="mb-3 relative">
+                        <div className="absolute -top-6 -left-2">
+                            <span className="bg-gray-200 px-1 rounded shadow-lg text-xs text-gray-600 font-semibold">
+                                {category}
+                            </span> 
+                        </div>
+                        <div className="-top-4.5 -right-2 absolute bg-gray-200 px-1 py-0.5 rounded">
+                            <CgShare size={20}/>
+                        </div>
                         <Link to={`/product/${category}/${productId}`}>
-                            <p className="font-semibold mb-2 line-clamp-1 cursor-pointer hover:text-gray-600 ">
+                            <p className="
+                                font-semibold 
+                                max-sm:text-xl 
+                                max-md:text-lg
+                                max-lg:text-md
+                                max-xl:text-sm
+                                max-2xl:text-lg
+                                2xl:text-xl
+                                leading-10
+                                text-gray-900
+                                py-2 
+                                line-clamp-1 
+                                cursor-pointer 
+                                hover:text-blue-600"
+                            >
                                 {name}
                             </p>
                         </Link>
-                        <div className="flex gap-1 items-center">
-                            <BsStarFill size={10} className="text-black"/>
-                            <BsStarFill size={10} className="text-black"/>
-                            <BsStarFill size={10} className="text-black"/>
-                            <BsStarFill size={10} className="text-black"/>
-                            <BsStarHalf size={10} className="text-black"/>
-                            <span className="text-xs">{price*16} reviews)</span>
-                            <span className="mx-1 h-4 w-0.5 bg-gray-300"></span><span className="font-bold text-sm">4.5</span>
-                        </div>
-                        <div className="flex gap-5 items-center justify-center font-bold mt-2">
-                            
-                            <p className="flex items-center justify-center leading-2 gap-1">
-                                <span className="text-sm">
-                                    {currency || 'ETB'}
-                                </span>
-                                <span className="text-lg text-gray-900">
-                                    {price*20 - price*20*0.25}
-                                </span>
+                       
+                        <div className="flex justify-between items-center mt-2">
+                            <p className="flex flex-col items-center font-semibold text-gray-500">
+                                <span className="max-sm:text-xs">{currency || 'ETB'}</span>
+                                <span className="line-through">{price*30}.00</span>
                             </p>
-                            <p className="flex flex-col text-[10px]">
-                                <span className="text-red-600">
-                                    - 25% now save ETB {price*20*0.25}
-                                </span>
-                                <span className="discountPrice text-gray-500">
-                                    {currency || 'ETB'}{price*20-1}.00
-                                </span>
+                            <p className="flex flex-col items-center font-bold text-gray-900">
+                                <span className="max-sm:text-xs">{currency || 'ETB'}</span>
+                                <span className="text-2xl text-gray-800">{price*30 - price*30*0.25-0.01}</span>
                             </p>
+                            <p className="bg-red-500 text-white font-semibold px-2 py-2 text-sm rounded-lg">25% OFF</p>
                         </div>
+                    </div>
+
+                    <div className="flex gap-1 items-center text-gray-600">
+                        <BsStarFill size={15} className="text-amber-500"/>
+                        <BsStarFill size={15} className="text-amber-500"/>
+                        <BsStarFill size={15} className="text-amber-500"/>
+                        <BsStarFill size={15} className="text-amber-500"/>
+                        <BsStarHalf size={15} className="text-amber-500"/>
+                        <span className="text-xs">({price*2} reviews)</span>
+                        <span className="mx-1 h-4 w-0.5 bg-gray-300"></span>
                     </div>
 
                     <div className="flex justify-center items-center">
@@ -102,14 +114,15 @@ function ProductItem({ name, productId, imgURL, price, category }: Props) {
                                 addToCart(productId)
                             }}
                             className="
-                            w-[80%] 
+                            w-full 
+                            mt-4 
                             bg-blue-600
                             text-gray-100
                             hover:bg-blue-700
                             font-semibold
-                            text-sm
-                            py-3
-                            rounded-lg
+                            text-lg
+                            py-4
+                            rounded-2xl
                             transition-all 
                             duration-100 
                             flex items-center 
@@ -118,7 +131,7 @@ function ProductItem({ name, productId, imgURL, price, category }: Props) {
                             group-hover:shadow-lg 
                             cursor-pointer"
                         >
-                            <ShoppingBag size={20} />
+                            <BiCart size={25} />
                             <span>ADD TO CART</span>
                         </button>
 
