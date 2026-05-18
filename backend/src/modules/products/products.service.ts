a5b1ade9b6ba61492689cdfc41d0308d1aef00ee
Sana Matusala <matusalasana@gmail.com>
2026-05-18 22:38:15 +0300

    prducte update: undefined values filtered from products data

--- backend/src/modules/products/products.service.ts
+++ backend/src/modules/products/products.service.ts
@@ -1,6 +1,9 @@
 import { CacheService } from '../../utils/CacheService';
 import { ProductFilters } from './products.validation';
-import { CreateProductInput } from './products.validation';
+import { 
+  CreateProductInput,
+  UpdateProductInput 
+} from './products.validation';
 import {
   getProductsRepo,
   getProductByIdRepo,
@@ -80,13 +83,56 @@ export const createProductService = async (
 
 
 // UPDATE
-export const updateProductService = async (id: string, data: any) => {
-  const product = await updateProductRepo(id, data);
+export const updateProductService = async (
+  id: string,
+  data: UpdateProductInput
+) => {
+
+  const productData: Record<string, any> = {};
+
+  // ONLY ADD FIELDS THAT EXIST VALIDATION (PATCH METHOD)
+  
+  if (data.name !== undefined) {
+    productData.name = data.name;
+  }
+
+  if (data.category_id !== undefined) {
+    productData.category_id = data.category_id;
+  }
+
+  if (data.description !== undefined) {
+    productData.description = data.description;
+  }
+
+  if (data.rating_count !== undefined) {
+    productData.rating_count = data.rating_count;
+  }
+
+  if (data.average_rating !== undefined) {
+    productData.average_rating = data.average_rating;
+  }
+
+  if (data.is_featured !== undefined) {
+    productData.is_featured = data.is_featured;
+  }
+
+  if (data.is_bestseller !== undefined) {
+    productData.is_bestseller = data.is_bestseller;
+  }
+  
+  
+
+  const updatedProduct = await updateProductRepo(
+    id,
+    productData,
+    data.images,
+    data.variants
+  );
 
   await CacheService.del(`product:${id}`);
   await CacheService.delByPattern('products:*');
 
-  return product;
+  return updatedProduct;
 };
 
 
