83395fca260d5d788fe671a394576c776ba10d24
Sana Matusala <matusalasana@gmail.com>
2026-05-18 22:34:45 +0300

    prducte update: validation defined

--- backend/src/modules/products/products.validation.ts
+++ backend/src/modules/products/products.validation.ts
@@ -30,13 +30,32 @@ export const createProductSchema = z.object({
 });
 
 export const updateProductSchema = z.object({
-  name: z.string().min(2).optional(),
-  description: z.string().optional(),
-  price: z.number().positive().optional(),
-  stock: z.number().int().nonnegative().optional(),
+  id: z.string().uuid().min(1, "Id of the product you're trying to update is required"),
   category_id: z.string().uuid().optional(),
+  name: z.string().min(2),
+  description: z.string(),
+  average_rating: z.number().positive(),
+  rating_count: z.int().nonnegative(),
   is_featured: z.boolean().optional(),
-  is_recommended: z.boolean().optional(),
+  is_bestseller: z.boolean().optional(),
+  
+  variants: z.array(
+    z.object({
+      product_id: z.string().uuid().optional(),
+      color: z.string().min(1, "Color is required"),
+      size: z.string().min(1, "Size is required"),
+      price: z.number().positive().min(100, "Amount must be greater than 100 ETB"),
+      stock_quantity: z.number().int().nonnegative(),
+      sku: z.string().min(1, "sku is required")
+  })),
+  
+  images: z.array(
+    z.object({
+      product_id: z.string().uuid().optional(),
+      url: z.string().min(1, "Image is required"),
+    })
+  ),
+  
 });
 
 export interface ProductFilters {
