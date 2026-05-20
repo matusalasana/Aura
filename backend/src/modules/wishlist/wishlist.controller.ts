7cc7448cd6164812a78f8e0238567a5300312df8
Sana Matusala <matusalasana@gmail.com>
2026-05-20 23:20:09 +0300

    feat: implement remove wishlist item

--- backend/src/modules/wishlist/wishlist.controller.ts
+++ backend/src/modules/wishlist/wishlist.controller.ts
@@ -2,12 +2,13 @@ import { Request, Response } from 'express';
 import { WishlistService } from './wishlist.service';
 
 
+// GET ALL
 const getWishlistItems = async (req: Request, res: Response) => {
   try{
     const list = await WishlistService.getAll(req.user!.id);
-  return res
-    .status(200)
-    .json(list);
+    return res
+      .status(200)
+      .json(list);
   }catch(err){
     console.log("Get all wishlist items:", err.message);
     res.status(500).json({
@@ -16,25 +17,38 @@ const getWishlistItems = async (req: Request, res: Response) => {
   };
 };
 
+// ADD 
 const addToWishlist = async(req: Request, res: Response) => {
   try{
     const newWishlistItem = await WishlistService.add(
       req.user!.id,
       req.body.product_id
     );
-  return res
-    .status(201)
-    .json(newWishlistItem);
+    return res
+      .status(201)
+      .json(newWishlistItem);
   }catch(err){
     console.log("Add to wishlist err:", err.message);
-    res.status(500).json({
+    res.status(401).json({
       message: `Add to wishlist err: ${err.message}`
     });
   };
 };
 
-const removeFromWishlist = asyncHandler(async (req: Request, res: Response) => {
-  await WishlistService.removeFromWishlist(req.user!.id, req.params.productId);
-  return res.status(200).json(new ApiResponse(200, {}, 'Removed from wishlist'));
-});
-}
+// REMOVE 
+const removeFromWishlist = async (req: Request, res: Response) => {
+  try{
+    const newWishlistItem = await WishlistService.remove(
+      req.user!.id,
+      req.body.product_id
+    );
+  return res
+    .status(200)
+    .json({message: "item removed from wishlist"});
+  }catch(err){
+    console.log("Remove wishlist err:", err.message);
+    res.status(401).json({
+      message: `Remove wishlist err: ${err.message}`
+    });
+  };
+};
