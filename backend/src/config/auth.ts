9074ec9ecd4ece0c0608df832124ff2f0f97d961
Sana Matusala <matusalasana@gmail.com>
2026-09-06 08:45:37 +0300

    chore: setup better auth for authentication, fix bugs

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { db } from "../db";
+import * as schema from "../db/schema";
+import { Env } from "./env";
+
+
+export const auth = betterAuth({
+  
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+
+  secret: Env.BETTER_AUTH_SECRET,
+
+  baseURL: Env.BETTER_AUTH_URL,
+
+  emailAndPassword: {
+    enabled: true,
+  },
+  
+});
